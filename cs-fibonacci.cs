be2e078144719f38f98fe32ffed6774fe1d13914
Diego / Alp <150331968+fuckingdiego@users.noreply.github.com>
2023-11-11 19:05:04 +0300

    Update cs-fibonacci.cs

--- cs-fibonacci.cs
+++ cs-fibonacci.cs
@@ -2,12 +2,10 @@ int a = 1;
             int b = 1;
             int c;
  
-            Console.WriteLine(a); /* 1 yazar */  /* aşağıdaki yorum satırında alt alta bir yazar dememin sebebi bu kısımdır */
-            Console.WriteLine(b); /* 1 yazar */
+            Console.WriteLine(a);
+            Console.WriteLine(b);
  
-            for (int i = 1; i <= 8; i++) /* 8 adet sayıyı üst üste toplayarak gidecektir. Eğer burada sekiz yerine faklı rakam yazarsak o kadar toplama yapacaktır. */
-               /* ekran çıktısında başta alt alta 1 1 yazacaktır a ve b ye 1 değerlerine 1 verdiğimiz için */
-               /* 1+1= 2 daha sonra her çıkan sonuçlar bir önceki değeri toplayarak gidecektir. */
+            for (int i = 1; i <= 8; i++) 
             {
                 c = a + b;
                 a = b;
